4d8c44921cd4455188bb568e6e9cd41d54079950
Isabelle <isabelle.dahlstromm@gmail.com>
2026-01-31 16:18:46 +0100

    added json file

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const path = require("path")
 const app = express();
 
 // app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public/index.html"))
+//     res.send("Welcome")
 // })
 
 // Express static middleware:
 // serve the whole "public" folder
-// app.use(express.static("public"))
+app.use(express.static("public"))
+
+// app.get("/", (req, res) => {
+//     res.send("Hello there")
+// })
 
 app.listen(3000, () => console.log("Sever is running on http://localhost:3000"))
