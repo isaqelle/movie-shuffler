3325bb1593beabc218453fdf117fd86349c3d04a
Isabelle <isabelle.dahlstromm@gmail.com>
2026-02-01 14:08:24 +0100

    added poster to each film

--- public/script.js
+++ public/script.js
@@ -1,20 +1,22 @@
 document.getElementById("pickBtn").addEventListener("click", () => {
-    fetch("/random-movie")
-        .then(res => res.json())
-        .then(movie => {
-            document.getElementById("title").textContent =
-                `${movie.title} (${movie.year})`;
-            const link = document.getElementById("link");
-            link.href = movie.letterboxdUrl;
-            link.textContent = "View on LetterBoxd"
-  })  
-})
+  fetch("/random-movie")
+    .then(res => res.json())
+    .then(movie => {
+      document.getElementById("title").textContent =
+        `${movie.title} (${movie.year})`;
 
-const poster = document.getElementById("poster");
+      const link = document.getElementById("link");
+      link.href = movie.letterboxdUrl;
+      link.textContent = "View on Letterboxd";
 
-if (movie.poster) {
-    poster.src = movie.poster;
-    poster.style.display = "block";
-} else {
-    poster.style.display = "none"
-}
+        // movie poster
+      const poster = document.getElementById("poster");
+
+      if (movie.poster) {
+        poster.src = movie.poster;
+        poster.style.display = "block";
+      } else {
+        poster.style.display = "none";
+      }
+    });
+});
